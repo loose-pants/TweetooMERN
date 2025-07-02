8ff83f95895d4332139782b74201f9cd122b15cc
loose-pants <168727329+loose-pants@users.noreply.github.com>
2025-07-02 13:07:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'https://sunny-sopapillas-ba4022.netlify.app' : 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' ? 'https://tweetofront.netlify.app' : 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
